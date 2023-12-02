8278d2df4657648dbfbda8f3bf6298a1efca3972
Gisela <giselamata27@gmail.com>
2023-12-02 13:41:15 -0500

    Adding in generateMarkdown data to test if it works.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -29,9 +29,46 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
-`;
+  ${licenseBadge}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${licenseLink}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${licenseSection}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, you can reach me through my GitHub profile:
+  [GitHub - ${data.github}](https://github.com/${data.github})
+
+  Or contact me via email:
+  [${data.email}](mailto:${data.email})
+  `;
 }
 
 module.exports = generateMarkdown;
