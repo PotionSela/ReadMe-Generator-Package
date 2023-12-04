f2226d35d15a735591ed8a22541b7843949dba21
Gisela <giselamata27@gmail.com>
2023-12-04 10:08:34 -0500

    Added final questions, and cleaned up comments on index and generateMarkdown js files.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,11 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 
 
-// TODO: Create an array of questions for user input
+//Creating an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -27,16 +27,47 @@ const questions = [
       name: 'usage',
       message: 'How do you use your project?',
     },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'How can other contribute to your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How do you run tests for your project?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'dependencies',
+      message: 'List any dependencies your project requires:',
+    },
 ];
 
-// TODO: Create a function to write README file
+//Creating a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('README.md generated successfully!')
   );
 }
 
-// TODO: Create a function to initialize app
+//Creating a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
         const markdown = generateMarkdown(answers);
